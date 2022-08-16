2ec7c71db0cc4c7a0e4d09657f1119acf9ef78ab
bikcode <bikcodeh@gmail.com>
2022-08-16 18:13:18 -0500

    refactor for information fragment layout

--- app/src/main/java/com/example/tococd/presentation/screens/information/InformationAdapter.kt
+++ app/src/main/java/com/example/tococd/presentation/screens/information/InformationAdapter.kt
@@ -37,7 +37,7 @@ class InformationAdapter :
         private fun setUpExpandableState(information: InformationModel) {
             with(binding) {
                 val isExpandable = information.expandable
-                expandableLayout.visibility = if (isExpandable) View.VISIBLE else View.GONE
+                descriptionInformation.visibility = if (isExpandable) View.VISIBLE else View.GONE
                 touchInformation.visibility = if (!isExpandable) View.VISIBLE else View.GONE
             }
         }
@@ -47,32 +47,4 @@ class InformationAdapter :
         val binding = parent.viewBinding(CardLayoutBinding::inflate, false)
         return InformationVH(binding)
     }
-
-    //override fun onBindViewHolder(holder: InformationVH, position: Int) {
-    //holder.bind(getItem(position))
-//        if (position == 0) {
-//            holder.binding.expandableLayout.visibility = View.GONE
-//            holder.binding.touchInformation.visibility = View.GONE
-//            holder.binding.tittleInformation.textSize = 20.toFloat()
-//            holder.binding.informationCardView.setBackgroundResource(R.drawable.information_background_first)
-//            holder.binding.touchInformation.textSize = 0.toFloat()
-//            holder.binding.descriptionInformation.visibility = View.GONE
-    //holder.binding.informationCardView.layoutParams.height = 850
-
-//            val typeface: Typeface =
-//                holder.binding.root.resources.getFont(R.font.poppins_regular)
-//            holder.binding.tittleInformation.setTypeface(typeface, Typeface.NORMAL)
-//        } else {
-//            val typeface: Typeface =
-//                holder.binding.root.resources.getFont(R.font.montserrat_regular)
-//            holder.binding.tittleInformation.setTypeface(typeface, Typeface.BOLD)
-
-//            holder.binding.informationCardView.setBackgroundResource(R.drawable.information_background)
-//            holder.binding.tittleInformation.textSize = 23.toFloat()
-    //holder.binding.informationCardView.layoutParams.height = 415
-
-    //}
-
-    //holder.binding.informationCardView
-    //}
 }
