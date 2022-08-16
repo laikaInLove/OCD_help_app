02794016ccf880a825e892cad2612bb4ce6e0220
bikcode <bikcodeh@gmail.com>
2022-08-16 14:02:54 -0500

    remove unused extension function

--- app/src/main/java/com/example/tococd/utils/extension/View.kt
+++ app/src/main/java/com/example/tococd/utils/extension/View.kt
@@ -2,19 +2,9 @@ package com.example.tococd.utils.extension
 
 import android.view.View
 import android.widget.ImageView
-import androidx.annotation.RawRes
-import com.airbnb.lottie.LottieAnimationView
-import com.airbnb.lottie.LottieDrawable
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.example.tococd.R
-
-fun LottieAnimationView.likeAnimation(@RawRes animationId: Int, isLiked: Boolean): Boolean {
-    this.setAnimation(animationId)
-    this.repeatCount = LottieDrawable.INFINITE
-    this.playAnimation()
-    return !isLiked
-}
 
 fun ImageView.loadUrlImage(url: String) {
     Glide.with(this)
