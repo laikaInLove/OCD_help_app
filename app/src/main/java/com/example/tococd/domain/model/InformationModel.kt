1185c468f0bfd94b31700f7d0ce24212e347b5c6
Cami <jcamilouc1@gmail.com>
2022-08-20 12:40:59 -0500

    implements strings for spanish language

--- app/src/main/java/com/example/tococd/domain/model/InformationModel.kt
+++ app/src/main/java/com/example/tococd/domain/model/InformationModel.kt
@@ -1,9 +1,11 @@
 package com.example.tococd.domain.model
 
+import androidx.annotation.StringRes
+
 class InformationModel(
     val imageDescription: Int,
-    val tittleDescription: String,
-    val descriptionDescription: String,
+    @StringRes val titleDescriptionResId: Int,
+    @StringRes val descriptionResId: Int,
     val touchDescription: String,
     var expandable: Boolean = false) {
 }
