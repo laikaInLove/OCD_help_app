c66b22f1e7c23f05ef743a39fd3732eda6902fbc
bikcode <bikcodeh@gmail.com>
2022-08-26 19:24:45 -0500

    implements query language for psychologist

--- app/src/main/java/com/example/tococd/data/remote/service/APIService.kt
+++ app/src/main/java/com/example/tococd/data/remote/service/APIService.kt
@@ -3,9 +3,12 @@ package com.example.tococd.data.remote.service
 import com.example.tococd.data.remote.response.PsychologistsResponse
 import retrofit2.Response
 import retrofit2.http.GET
-import retrofit2.http.Url
+import retrofit2.http.Query
+import java.util.Locale
 
 interface APIService {
     @GET("psychologists")
-    suspend fun getPsychologists(): Response<List<PsychologistsResponse>>
+    suspend fun getPsychologists(
+        @Query("lang") lang: String = Locale.getDefault().language
+    ): Response<List<PsychologistsResponse>>
 }
